9c7d5b5b99470a8e0dd8f2683d4df033b0be8e13
Brent <bjgaynor@gmail.com>
2018-09-26 08:09:54 -0400

    flash messages working, manipulating lists easily now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  http_basic_authenticate_with name: "brent", password: "gaynor", except: :index
+  protect_from_forgery with: :exception
+
 end
--- app/models/movie_list.rb
+++ app/models/movie_list.rb
@@ -3,11 +3,11 @@ class MovieList < ApplicationRecord
   has_many :movies
 
   def number_of_movies
-    # ***
+    movies.count
   end
 
   def average_rating
-    # ***
+    # map or collect movies ratings and average them
   end
 
 end
